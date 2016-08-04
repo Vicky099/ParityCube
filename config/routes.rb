c6b3aebcda6a935d0c767e2b912e58fe6b93ca04
vikram Panmand <vikky.panmand@gmail.com>
2016-08-04 17:37:08 +0530

    [update] UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   #devise_for :users
   #root to: "home#index"
-  devise_for :users, :skip => [:registrations]
+  devise_for :users
   devise_scope :user do
     get '/users/signin' => "devise/sessions#new"
     get '/users/sign_out' => 'devise/sessions#destroy'
